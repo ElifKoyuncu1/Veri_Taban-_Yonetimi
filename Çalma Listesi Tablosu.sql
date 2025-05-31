4f922ed9939236a056baf067b98b6931a0d60689
Elif Koyuncu <elifkoyuncu713@gmail.com>
2025-05-31 16:37:12 +0300

    Delete Çalma Listesi Tablosu.sql

--- Çalma Listesi Tablosu.sql
+++
@@ -1,9 +0,0 @@
-create table CalmaLÝstesi(
-Liste_ID int primary key,
-Kullanýcý_ID int,
-Liste_adý varchar(50),
-Olusturan varchar(40),
-Gizlilik tinyint,  -- 1 = gizli, 0 = herkese açýk
-Sarki_ID int,
-foreign key (Kullanýcý_ID) references Kullanýcý(Kullanýcý_ID),
-foreign key (Sarki_ID) references Sarki(Sarki_ID))
